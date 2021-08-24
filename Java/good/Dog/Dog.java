73dd9c47c0c160d392e1a71dd81221a3551111af
Lin <xvcheng2020@163.com>
2021-08-24 16:58:10 +0800

    java

---
+++ Java/good/Dog/Dog.java
@@ -0,0 +1,10 @@
+package Java.good.Dog;
+
+public class Dog {
+    public Dog(String name) {
+        System.out.println("The dog name is: " + name );
+    }
+    public static void main(String[] args) {
+        Dog mydog = new Dog("Jilly");
+    }
+}
