0b7f17832da7f34a9a1f0623d746ece366098e4d
Lin <xvcheng2020@163.com>
2021-08-26 14:56:34 +0800

    python and GMP

---
+++ Python/IO/Fowrite.py
@@ -0,0 +1,5 @@
+fo = open("foo.txt", "w")
+fo.write( "www.runoob.com!\nVery good site!\n")
+ 
+# 关闭打开的文件
+fo.close()
--- Python/Test/Test.py
+++ Python/Test/Test.py
@@ -1,1 +1,19 @@
-print("Hello")
+import os
+
+print(os.getcwd())
+os.chdir("C:\\Users\\C5327170\\OneDrive - SAP SE\\Desktop\\notebook\\Python\\IO")
+print(os.getcwd())
+if(os.path.isdir("txt")):
+    pass
+else:
+    os.mkdir("txt")
+os.chdir("txt")
+print(os.getcwd())
+
+qq = open("qq.txt", "a+")
+content1 = qq.read(10)
+print(content1)
+qq.write( "UnicodeEncodeError: 'charmap' codec can't encode characters in position 0-46: character maps to <undefined>\n" )
+content2 = qq.read(20)
+print(content2)
+qq.close()
